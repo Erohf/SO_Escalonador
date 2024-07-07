3eaf803666bb59dc35f52a963fbfa4ff099b8ee0
Lucas <lucasssandrade183@gmail.com>
2024-07-07 19:26:10 -0300

    Alterando algoritmos.py

--- algoritmos.py
+++ algoritmos.py
@@ -8,9 +8,14 @@ class FIFO:
         tempo_atual = 0
         print("Executando escalonamento FIFO:")
         for processo in self.processos:
+            if processo.chegada > tempo_atual:
+                tempo_atual = processo.chegada
             processo.tempo = tempo_atual
-            tempo_atual += processo.execucao
-            print(f"Processo {processo.id} executado de {processo.tempo} até {tempo_atual}")
+            tempo_execucao = processo.execucao + self.sobrecarga
+            print(f"Processo {processo.id} executado de {tempo_atual} até {tempo_atual + processo.execucao}")
+            tempo_atual += tempo_execucao
+            if self.sobrecarga > 0:
+                print(f"Sobrecarga de {tempo_atual - self.sobrecarga} até {tempo_atual}")
 
 class SJF:
     def __init__(self, processos, quantum, sobrecarga):
@@ -22,9 +27,14 @@ class SJF:
         tempo_atual = 0
         print("Executando escalonamento SJF:")
         for processo in self.processos:
+            if processo.chegada > tempo_atual:
+                tempo_atual = processo.chegada
             processo.tempo = tempo_atual
-            tempo_atual += processo.execucao
-            print(f"Processo {processo.id} executado de {processo.tempo} até {tempo_atual}")
+            tempo_execucao = processo.execucao + self.sobrecarga
+            print(f"Processo {processo.id} executado de {tempo_atual} até {tempo_atual + processo.execucao}")
+            tempo_atual += tempo_execucao
+            if self.sobrecarga > 0:
+                print(f"Sobrecarga de {tempo_atual - self.sobrecarga} até {tempo_atual}")
 
 class RoundRobin:
     def __init__(self, processos, quantum, sobrecarga):
@@ -47,10 +57,14 @@ class RoundRobin:
                 tempo_atual += self.quantum
                 processo.execucao -= self.quantum
                 queue.append(processo)
+                if self.sobrecarga > 0:
+                    print(f"Sobrecarga de {tempo_atual - self.sobrecarga} até {tempo_atual}")
             else:
                 processo.tempo = tempo_atual
                 print(f"Processo {processo.id} executado de {tempo_atual} até {tempo_atual + processo.execucao}")
                 tempo_atual += processo.execucao
+                if self.sobrecarga > 0:
+                    print(f"Sobrecarga de {tempo_atual - self.sobrecarga} até {tempo_atual}")
 
 class EDF:
     def __init__(self, processos, quantum, sobrecarga):
@@ -60,6 +74,16 @@ class EDF:
 
     def executar(self):
         print("Executando escalonamento EDF:")
+        tempo_atual = 0
         for processo in self.processos:
-            processo.tempo = processo.chegada
-            print(f"Processo {processo.id} executado de {processo.tempo} até {processo.tempo + processo.execucao}")
+            if processo.chegada > processo.deadline:
+                print(f"Processo {processo.id} perdeu o deadline.")
+            else:
+                if processo.chegada > tempo_atual:
+                    tempo_atual = processo.chegada
+                processo.tempo = tempo_atual
+                tempo_execucao = processo.execucao + self.sobrecarga
+                print(f"Processo {processo.id} executado de {tempo_atual} até {tempo_atual + processo.execucao}")
+                tempo_atual += tempo_execucao
+                if self.sobrecarga > 0:
+                    print(f"Sobrecarga de {tempo_atual - self.sobrecarga} até {tempo_atual}")
