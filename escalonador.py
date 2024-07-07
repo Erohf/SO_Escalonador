450afa8b612a1f77a86653493f43632da8d933f7
Lucas <lucasssandrade183@gmail.com>
2024-07-07 18:17:31 -0300

    Iniciando projeto SO

---
+++ escalonador.py
@@ -0,0 +1,39 @@
+from objetos import *
+
+def main():
+    int_quantum = int(input("\nQual o quantum do sistema? "))
+
+    int_sobrecarga = int(input("\nQual a sobrecarga do sistema? "))
+
+    sistema = Sistema(int_quantum, int_sobrecarga)
+
+    int_processos = int(input("\nQual o número de processos? "))
+
+    processos = []
+
+    for i in range(int_processos):
+        int_id = i+1
+
+        int_chegada = int(input(f"\nQual o tempo de chegada do processo {i}? "))
+
+        int_execucao = int(input(f"\nQual o tempo de execução do processo {i}? "))
+
+        int_deadline = int(input(f"\nQual o deadline do processo {i}? "))
+
+        # print(f"Qual a prioridade do processo {i}?")
+        # int_prioridade = int(input())
+
+        processo = Processo(int_id, int_chegada, int_execucao, int_deadline)
+        processos.append(processo)
+
+        print(f"\nProcesso {i} criado com sucesso!")
+
+    escolha = Escolha()
+    print(f"\nVocê escolheu o algoritmo: {escolha.opcao}")
+
+    print("\nLista de Processos Armazenados:")
+    for processo in processos:
+        print(processo)
+
+if __name__ == '__main__':
+    main()
---
+++ objetos.py
@@ -0,0 +1,28 @@
+class Sistema:
+    def __init__(self, quantum, sobrecarga):
+        self.quantum = quantum
+        self.sobrecarga = sobrecarga
+
+class Processo:
+    def __init__(self, id, chegada, execucao, deadline, prioridade=None):
+        self.id = id
+        self.chegada = chegada
+        self.execucao = execucao
+        self.deadline = deadline
+        self.prioridade = prioridade
+
+    def __str__(self):
+        return (f"Processo ID: {self.id}, Chegada: {self.chegada}, Execução: {self.execucao}, "
+                f"Deadline: {self.deadline}")
+
+class Escolha:
+    def __init__(self):
+        print("\nQual o algoritmo de escalonamento?")
+        print("1 - FIFO")
+        print("2 - SJF")
+        print("3 - EDF")
+        print("4 - Round Robin")
+        self.opcao = int(input("\nEscolha uma opção (1-4): "))
+        while self.opcao not in [1, 2, 3, 4]:
+            print("\nOpção inválida. Tente novamente.")
+            self.opcao = int(input("Escolha uma opção (1-4): "))
